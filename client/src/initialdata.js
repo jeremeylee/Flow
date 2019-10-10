d9443d507b4213e38b6d560ca437155a3f716889
Jeremey Lee <leejeremey@gmail.com>
2019-10-09 17:35:19 -0700

    Modified task object to include title and content

--- client/src/initialdata.js
+++ client/src/initialdata.js
@@ -1,9 +1,25 @@
 const initialData = {
   tasks: {
-    'task-1': { id: 'task-1', content: 'Task 1'},
-    'task-2': { id: 'task-2', content: 'Task 2'},
-    'task-3': { id: 'task-3', content: 'Task 3'},
-    'task-4': { id: 'task-4', content: 'Task 4'},
+    'task-1': { 
+      id: 'task-1', 
+      title: 'Task 1', 
+      content: "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis dolor vitae lectus pharetra ullamcorper. Morbi sed turpis sapien. Morbi tristique massa vitae nibh pharetra, nec rhoncus urna suscipit. Pellentesque vel turpis consequat, vestibulum dolor ullamcorper, ultrices quam. Donec tempus tristique mauris, sed rutrum metus ultrices non." 
+    },
+    'task-2': { 
+      id: 'task-2', 
+      title: 'Task 2',
+      content: "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis dolor vitae lectus pharetra ullamcorper. Morbi sed turpis sapien. Morbi tristique massa vitae nibh pharetra, nec rhoncus urna suscipit. Pellentesque vel turpis consequat, vestibulum dolor ullamcorper, ultrices quam. Donec tempus tristique mauris, sed rutrum metus ultrices non." 
+    },
+    'task-3': { 
+      id: 'task-3', 
+      title: 'Task 3',
+      content: "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis dolor vitae lectus pharetra ullamcorper. Morbi sed turpis sapien. Morbi tristique massa vitae nibh pharetra, nec rhoncus urna suscipit. Pellentesque vel turpis consequat, vestibulum dolor ullamcorper, ultrices quam. Donec tempus tristique mauris, sed rutrum metus ultrices non." 
+    },
+    'task-4': { 
+      id: 'task-4', 
+      title: 'Task 4',
+      content: "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis dolor vitae lectus pharetra ullamcorper. Morbi sed turpis sapien. Morbi tristique massa vitae nibh pharetra, nec rhoncus urna suscipit. Pellentesque vel turpis consequat, vestibulum dolor ullamcorper, ultrices quam. Donec tempus tristique mauris, sed rutrum metus ultrices non." 
+    },
   },
 
   processes: {
