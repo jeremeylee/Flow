1c0d273df3186126f110c4b31f5241782b09dbe9
Jeremey Lee <leejeremey@gmail.com>
2019-10-09 17:34:26 -0700

    Created Task component

---
+++ client/src/components/Task.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TaskModal from './TaskModal';
+
+const Task = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  }
+
+  return (
+    <Draggable draggableId={props.task.id} index={props.index} key={props.task.id} isDragDisabled={open}>
+    {(provided) => (
+      <ListItem
+        innerRef={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+      >
+      <ListItemText onClick={handleOpenModal} primary={props.task.title} />
+      <TaskModal
+        open={open}
+        handleClose={handleCloseModal}
+        task={props.task}
+      />
+    </ListItem>
+    )}
+  </Draggable>
+  )
+}
+
+export default Task;
